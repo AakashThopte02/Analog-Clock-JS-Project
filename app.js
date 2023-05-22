a938bfa0c3c0074a11fbc47883756efe37da45da
Akash Ashok Thopate <124392891+AakashThopte02@users.noreply.github.com>
2023-05-22 23:36:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+setInterval(() => {
+  date = new Date();
+  htime = date.getHours();
+  mtime = date.getMinutes();
+  stime = date.getSeconds();
+  hrotation = 30 * htime + mtime / 2;
+  mrotation = 6 * mtime;
+  srotation = 6 * stime;
+  // let hour = document.querySelector("#hour");
+  // let minute = document.querySelector("#minute");
+  // let second = document.querySelector("#second");
+  hour.style.transform = `rotate(${hrotation}deg)`;
+  minute.style.transform = `rotate(${mrotation}deg)`;
+  second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
